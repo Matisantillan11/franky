fff6b84c326ecbbb6fc8cc5e7d5e214e85e78276
matisantillan11 <matias1.santillan@gmail.com>
2026-02-18 21:16:45 -0300

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,10 @@
+// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
-  ...expoConfig,
+  expoConfig,
   {
-    settings: {
-      'import/resolver': {
-        typescript: {
-          project: './tsconfig.json',
-        },
-      },
-    },
-  },
+    ignores: ["dist/*"],
+  }
 ]);
